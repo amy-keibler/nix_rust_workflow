404b804caba304d824ae8f54fc88a81d2d562a75
Amy Keibler <amelia.keibler@gmail.com>
2023-02-20 16:10:48 -0500

    Create a package with naersk

--- flake.nix
+++ flake.nix
@@ -3,14 +3,32 @@
 
   inputs = {
     utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs, utils, naersk }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
+        naersk' = pkgs.callPackage naersk { };
+
+        # library / tool dependencies for runtime
+        plottersBuildInputs = with pkgs; [ fontconfig ];
+        # library / tool dependencies for build time
+        plottersNativeBuildInputs = with pkgs; [ cmake pkg-config ];
+        # bundled for the development shell for convenience
+        plottersPackages = plottersBuildInputs ++ plottersNativeBuildInputs;
       in
       rec {
+        # define a package using naersk to build the Rust code
+        packages.nixRustWorkflow = naersk'.buildPackage {
+          src = ./.;
+          buildInputs = plottersBuildInputs;
+          nativeBuildInputs = plottersNativeBuildInputs;
+        };
+
+        # set the default package to the one we defined
+        packages.default = packages.nixRustWorkflow;
 
         # define the default development shell environment
         devShells.default = pkgs.mkShell {
@@ -25,14 +43,9 @@
             rustfmt
             rust-analyzer
 
-            # plotters dependencies
-            cmake
-            fontconfig
-            pkg-config
-
             # formatter for flakes
             nixpkgs-fmt
-          ];
+          ] ++ plottersPackages;
 
           # set an environment variable for the standard library path
           # required for rust-analyzer
